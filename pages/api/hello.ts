4a157cb360b692bc8ce52cce14bb393ef41d1492
Amirkhanov Daniil Igorevich <damirkhanov@ozon.ru>
2023-02-11 22:29:38 +0300

    [UI-4]: danil ne loh

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,6 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-	console.log('Danil loh')
+	console.log('Danil ne loh')
   res.status(200).json({ name: 'John Doe' })
 }
