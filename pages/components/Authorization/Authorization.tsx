25e11f616236ab65749a3eb467555751d287ccee
Anton <anton.gap99@gmail.com>
2023-02-24 01:57:03 +0300

    FE-1: create Authorization

--- pages/components/Authorization/Authorization.tsx
+++ pages/components/Authorization/Authorization.tsx
@@ -1,10 +1,27 @@
+import Image from 'next/image';
 import style from './Authorization.module.css';
-
+import svg from './images/logo.svg'
 type Props = {}
 
 
-export const Authorization = (props:Props) => {
+export const Authorization = (props: Props) => {
 
-  return <div className = {style.container}></div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.imageWrapper}>
+        <Image src={svg} alt="logo e-education"/>
+      </div>
+
+      <form className={style.form}>
+        <h3 className={style.title}>Вход</h3>
+        <label htmlFor="1" className={style.labelEmail}>Почта</label>
+        <input type="text" id='1' placeholder='productname@gmail.ru' name='email' className={style.input}/>
+        <label htmlFor="1" className={style.labelEPassword}>Пароль</label>
+        <input type="text" id='1' placeholder='productname@gmail.ru' name='password' className={style.input}/>
+        <button type="submit">Войти</button>
+        <p className={style.info}>Если у вас нет аккаунта, вы можете <a href='/' className={style.regLink}>зарегистрироваться</a></p>
+      </form>
+    </div>
+  )
 }
 
