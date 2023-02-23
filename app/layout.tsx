f7fb32dd0b17469866945f529c884af401eaee8d
Anton <anton.gap99@gmail.com>
2023-02-24 02:25:31 +0300

    FE-1 some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import { Inter } from '@next/font/google';
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
   display: 'swap',
 })
 
--- pages/components/Authorization/Authorization.tsx
+++ pages/components/Authorization/Authorization.tsx
@@ -14,11 +14,11 @@ export const Authorization = (props: Props) => {
 
       <form className={style.form}>
         <h3 className={style.title}>Вход</h3>
-        <label htmlFor="1" className={style.labelEmail}>Почта</label>
+        <label htmlFor="1" className={style.label}>Почта</label>
         <input type="text" id='1' placeholder='productname@gmail.ru' name='email' className={style.input}/>
-        <label htmlFor="1" className={style.labelEPassword}>Пароль</label>
-        <input type="text" id='1' placeholder='productname@gmail.ru' name='password' className={style.input}/>
-        <button type="submit">Войти</button>
+        <label htmlFor="1" className={style.label}>Пароль</label>
+        <input type="text" id='1' placeholder='••••••' name='password' className={style.input}/>
+        <button type="submit" className={style.btn}>Войти</button>
         <p className={style.info}>Если у вас нет аккаунта, вы можете <a href='/' className={style.regLink}>зарегистрироваться</a></p>
       </form>
     </div>
